758338d56ae5b1abe0efe9a961a19e09c53ee092
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 07:54:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -24,7 +24,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
  
-    if src[row_index][element_index].length > longest_string_element.length
+    if 
       longest_string_element = spice_rack[row_index][element_index]
     end
     element_index += 1
